91703519752177c3a90a401cdd21d5ca0275b100
Iamneo <techadmin@iamneo.ai>
2023-07-31 16:57:05 +0000

    Backup Commit

--- react-crud-with-json-server-master/src/http-common.js
+++ react-crud-with-json-server-master/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://8080-adaebadfaafeacdeacafbadecbcafddeada.project.examly.io",
     headers: {
         "Content-type": "application/json"
     }
