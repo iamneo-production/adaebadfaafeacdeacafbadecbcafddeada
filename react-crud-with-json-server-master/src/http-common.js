5aab1eb951c76bd9515d14a3264f07f04baaaac0
Iamneo <techadmin@iamneo.ai>
2023-07-31 17:30:49 +0000

    Backup Commit

--- react-crud-with-json-server-master/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://8080-adaebadfaafeacdeacafbadecbcafddeada.project.examly.io",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
